ec51c6bb66b990247170d01c86b856dfcaf3c609
Mikael Thorup <mhthorup@gmail.com>
2023-01-17 22:45:48 +0100

    fixed a few things and added the rest of the story

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # meow
 🐈
+
+`$ chmod -x meow.sh` to enable the file to run (after checking that the code doesn't do all kinds of funky stuff to your computer 👩‍💻)
+`$ ./meow.sh` to start the game!
--- meow.sh
+++ meow.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
+
 clear -x
 printf "Welcome to the cat game!\n\n"
 echo "   |\---/|"
 echo "   | ,_, |"
 echo "    \_v_/-..----."
-echo " ___/ '   ' ,''+ \  sk"
+echo " ___/ '   ' ,''+ \  "
 echo "(__...'   __\    |\.___.';"
 echo "  (_,...'(_,.'__)/'.....+"
 echo " "
@@ -14,7 +15,7 @@ clear -x
 
 #initialize variables
 GAME_RUNNING=true
-PARAGRAPH_NUMBER=100
+USER_CHOICE=100
 INVALID_SELECTION=false
 SCRATCHES=0
 PURR_REACTION="It likes it"
@@ -22,7 +23,7 @@ PURR_REACTION="It likes it"
 function make_your_choice() {
     #sleep 0.5
     printf " \n\nWhat do you do?\n$1\n"
-    read -p "Selection: " PARAGRAPH_NUMBER
+    read -p "Selection: " USER_CHOICE
     #do some choice checking here to ensure the inputs match expected ones. 
     clear -x
 }
@@ -30,26 +31,26 @@ function make_your_choice() {
 while $GAME_RUNNING
 do 
 
-case "$PARAGRAPH_NUMBER" in
+case "$USER_CHOICE" in
     100)
         printf "You're walking in a quiet street, it is light outside, and you are enjoying the weather. You need to hurry up, as you need to make your appointment."
         make_your_choice "101: hurry up\n102: walk in a relaxed pace."
-        if [ "$PARAGRAPH_NUMBER" == "101" ]
+        if [ "$USER_CHOICE" == "101" ]
             then
                 ANSWER="hurry up"
             else
                 ANSWER="walk in a relaxed pace"
         fi
-        PARAGRAPH_NUMBER=101 #illusion of choice muahaha.
+        USER_CHOICE=101 #illusion of choice muahaha.
         ;;
     101)
-        printf "You $ANSWER. After a while, you see a cat on the road. It's a cute cat. You walk closer to the cat."
+        printf "You $ANSWER. After a while, you see a cat on the road. It looks like a cute cat."
         make_your_choice "110: pet the cat\n120: walk near the cat\n999: hurry up!"
         ;;
     110)
         printf "You pet the cat. $PURR_REACTION!"
         make_your_choice "110: Keep petting the cat\n120: Say goodbye to the kitty and walk on by."
-        if [ "$PARAGRAPH_NUMBER" -eq "110" ]
+        if [ "$USER_CHOICE" -eq "110" ]
             then
                 ((SCRATCHES++))
                 case $SCRATCHES in
@@ -78,20 +79,54 @@ case "$PARAGRAPH_NUMBER" in
                         PURR_REACTION="You really should be going now, to make your appointment" 
                         ;;
                 esac
-                echo "$SCRATCHES"
+            else 
+                :
         fi
+        ;;
+    120)
+        if [ "$SCRATCHES" -eq "0" ]
+            then
+                IS_ALREADY_PETTED="Go over and pet the cat"
+            else
+                IS_ALREADY_PETTED="Go back and pet the cat some more"
+        fi
+        printf "You hear the cat meowing, but you really need to hurry up. But the cat just looks so cute!"
+        make_your_choice "110: $IS_ALREADY_PETTED\n130: Say goodbye to the kitty and walk on by."
+        if [ "$USER_CHOICE" -eq "130" ] && [ "$SCRATCHES" -eq "0" ]
+        then
+            USER_CHOICE=900
+        elif [ "$USER_CHOICE" -eq "130" ] && [ "$SCRATCHES" -lt "5" ]
+        then
+            USER_CHOICE=910
+        elif [ "$USER_CHOICE" -eq "130" ]
+        then
+            USER_CHOICE=920
+        else
+            :
+        fi
+        ;;
+    900)
+        printf "you hurry up, and make it on to the bus. You look out of the window, and see the cat. The cat is sad, and you feel bad."
+        make_your_choice "101: Play again\nquit: quit game"
+        ;;
+    910)
+        printf "you hurry up. You get on your bus, and think of the cat. You hope the cat is happy and appreciates your scritches. You wonder what would happen if you petted it some more."
+        make_your_choice "101: Play again\nquit: quit game"
+        ;;
+    920)
+        printf "you hurry up. You don't make the bus, and send a message letting your friend know you're gonna be late. He texts back that it's okay. The cat apparently followed you, and you play with him until your bus arrives."
+        make_your_choice "101: Play again\nquit: quit game"
         ;;
     template)
         printf ""
         make_your_choice ""
         ;;
-    
     quit)
         GAME_RUNNING=false
         ;;
     *)
-        printf "you chose $PARAGRAPH_NUMBER, which is not a valid number." 
-        read -p "Please write a correct selection, or write quit to quit the game: " PARAGRAPH_NUMBER
+        printf "you chose $USER_CHOICE, which is not a valid number." 
+        read -p "Please write a correct selection, or write quit to quit the game: " USER_CHOICE
         ;;
 esac
 
